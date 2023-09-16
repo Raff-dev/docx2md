f64b9242119d22b2cce2dc8905b026fcba27032a
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-09-16 16:04:05 +0200

    display info on no files to change

--- docx2md/main.py
+++ docx2md/main.py
@@ -8,11 +8,14 @@ def main():
     repo = git.Repo(os.getcwd())
     index = repo.index
     changed_files = [item.a_path for item in index.diff("HEAD")]
+    if not changed_files:
+        print("No files to convert.")
+        return
 
     for file in changed_files:
         if file.endswith(".docx"):
             md_file = file.replace(".docx", ".md")
-            pypandoc.convert_file(file, "markdown", outputfile=md_file)
+            pypandoc.convert_file(file, "html", outputfile=md_file)
             index.add([md_file])
             print(f"Converted {file} to {md_file} and added to commit.")
 
