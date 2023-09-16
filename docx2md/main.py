9b2b1e4d2e494fcfaaecd73ca02ef45dcb2f7dc2
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-09-16 16:10:55 +0200

    proper return if no files match

--- docx2md/main.py
+++ docx2md/main.py
@@ -7,18 +7,17 @@ import pypandoc
 def main():
     repo = git.Repo(os.getcwd())
     index = repo.index
-    changed_files = [item.a_path for item in index.diff("HEAD")]
+    changed_files = [item.a_path for item in index.diff("HEAD") if item.a_path.endswith(".docx")]
     if not changed_files:
         print("No files to convert.")
         return
 
     print("Converting files to .md and adding to commit:")
     for file in changed_files:
-        if file.endswith(".docx"):
-            md_file = file.replace(".docx", ".md")
-            pypandoc.convert_file(file, "html", outputfile=md_file)
-            index.add([md_file])
-            print(f"\t- {file} -> {md_file}")
+        md_file = file.replace(".docx", ".md")
+        pypandoc.convert_file(file, "html", outputfile=md_file)
+        index.add([md_file])
+        print(f"\t- {file} -> {md_file}")
 
 
 if __name__ == "__main__":
